2a5553cd2cd60c8bbd8d392e0f66c83de93d786a
Paulo Vítor <paulovmedeiros@outlook.com>
2022-06-19 18:33:40 +0000

    Commenting code sections for better understanding Assignment 2 #4

--- answers/assignment 2/assignment2.py
+++ answers/assignment 2/assignment2.py
@@ -3,7 +3,6 @@ num1 = int(0)
 num2 = int(1)
 num3 = int(0)
 
-
 print("# Sequência de Fibonacci #")
 print("Consulte se um número pertence à Sequência de Fibonacci.")
 
@@ -11,17 +10,19 @@ print("Consulte se um número pertence à Sequência de Fibonacci.")
 numDigitado = int(input("Digite um número:"))
 
 # Calculando a sequência com base no número informado
-
 while numDigitado > num3:
     num3 = num1 + num2
     num1 = num2
     num2 = num3
 
-if numDigitado == 0:
+# Caso o número seja igual a zero ou 1:
+if numDigitado == 0 | numDigitado == 1:
     print("O número " + str(numDigitado) + " está na sequência de Fibonacci.")
 
+# Caso o número digitado seja igual ao número gerado na sequência, ele pertence à sequência.
 elif numDigitado == num3:
     print("O número " + str(numDigitado) + " está na Sequência de Fibonacci.")
 
+# Exceções a estes dois casos não pertencem à sequência de Fibonacci.
 else:
-    print("O número não está presente na Sequência de Fibonacci")
+    print("O número não está presente na Sequência de Fibonacci.")
